d634e43808187b4fa9940151685917896fdb8dae
denpufa <47856330+denpufa@users.noreply.github.com>
2021-01-13 23:52:47 -0300

    meta chars

--- meta_chars.py
+++ meta_chars.py
@@ -1,20 +1,34 @@
 import re
 # . ^ $ * + ? { } [ ] \ | ( )
-
-s = ' abc\n'
+s = ' abc'
 # . any caracter except new line caracter
 for x in re.finditer(r'.',s) :
     print(x)
 # ^ start with "^comeco"
 for x in re.finditer(r'^ a',s) :
     print(x)
-
 # $ ends with "fim$"
+for x in re.finditer(r'c$',s) :
+    print(x)
 # * zero or more occurrences "a*"
+for x in re.finditer(r'd*',s) :
+    print(x)
 # + one or more occurrences "a+"
+for x in re.finditer(r'a+b+c+',s) :
+    print(x)
 # { } Exactly the specified number of occurrences "al{2}"
+for x in re.finditer(r'.{4}',s) :
+    print(x)
 # [] a set of caracteres "[a-m]"
+for x in re.finditer(r'[a-c]',s):
+    print(x)
 # \ special sequence or caracter "\d"
+    #in special.py
 # | Either or "a|b"
+for x in re.finditer(r'a|b',s) :
+    print(x)
 # () capture and group 
+for x in re.finditer(r'(a)',s) :
+    print(x)
+
 
